c67a116b7e92aa4f4d4468cab85e2ab3b560a57b
leessmin <1442772970@qq.com>
2025-09-23 21:29:48 +0800

    init: 初始化前端项目

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,5 +4,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	server: {
+		proxy: {
+			"/api": "http://100.93.208.87:8080",
+			"/upload": "http://100.93.208.87:8080",
+			"/assert": "http://100.93.208.87:8080"
+		}
+	}
 });
