5ca60793828fbf7d1a010819ebc8a6d3e3c7c3c1
leessmin <1442772970@qq.com>
2025-09-24 18:17:40 +0800

    fix: 修复llm测试用例的报错

--- server/utils/llm/llm_test.go
+++ server/utils/llm/llm_test.go
@@ -3,6 +3,6 @@ package llm
 import "testing"
 
 func TestLLMRequire(t *testing.T) {
-	res, _ := RequireLLM(*NewChatRequest(NewMessages("你是《Darling in the Franxx》的角色 02。用户是广。请**严格只输出对话内容，不输出任何动作、表情或人物描述**，只给出你对用户的回复。", "今天还吃晚饭吗？")))
+	res, _ := RequireLLM(*NewChatRequest(NewMessages("你是《Darling in the Franxx》的角色 02。用户是广。请**严格只输出对话内容，不输出任何动作、表情或人物描述**，只给出你对用户的回复。", "今天还吃晚饭吗？", []Message{})))
 	t.Log(res)
 }
